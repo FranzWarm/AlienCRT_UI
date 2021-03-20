30d75badc2a4ab14ee6b05ff5b9ea8d94b580196
FranzWarm <franciscowarmling@gmail.com>
2021-03-19 21:04:22 -0300

    v0.5
    New chat card style.
    Fixed a bug in the character sheets css that was interfering with items sheets.

--- dist/module/init.js
+++ dist/module/init.js
@@ -1,7 +1,7 @@
 import { aliencrtActorSheet } from "./sheet/character-sheet.js";
 import { aliencrtSynthActorSheet } from "./sheet/synth-sheet.js";
 
-Hooks.once("init", () => {
+//Hooks.once("init", () => {
     Actors.registerSheet("alienrpg", aliencrtActorSheet, { types: ["character"], makeDefault: true }); //makeDefault: true is what makes this alternate sheet the default sheet when creating an "actor".
 	Actors.registerSheet("alienrpg", aliencrtSynthActorSheet, { types: ["synthetic"], makeDefault: true });
-});
+//});
