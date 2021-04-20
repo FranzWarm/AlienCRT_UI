5f486c177d5e2e12670e2f341c40baf65c6dc061
FranzWarm <franciscowarmling@gmail.com>
2021-04-20 14:09:31 -0300

    v 0.5.4
    This version updates the order of the cosumables in the general tab, just like the original sheet.
    
    Thanks to Simon Lee (simlee009) it also fixes some issues with relative paths for images.

--- dist/module/init.js
+++ dist/module/init.js
@@ -1,7 +1,5 @@
 import { aliencrtActorSheet } from "../module/sheet/character-sheet.js";
 import { aliencrtSynthActorSheet } from "../module/sheet/synth-sheet.js";
 
-//Hooks.once("init", () => {
     Actors.registerSheet("alienrpg", aliencrtActorSheet, { types: ["character"], makeDefault: true }); //makeDefault: true is what makes this alternate sheet the default sheet when creating an "actor".
 	Actors.registerSheet("alienrpg", aliencrtSynthActorSheet, { types: ["synthetic"], makeDefault: true });
-//});
