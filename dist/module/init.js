232b595f02f461ce33b0ff043e6762582a7005df
FranzWarm <franciscowarmling@gmail.com>
2021-06-14 10:59:11 -0300

    0

--- dist/module/init.js
+++
@@ -1,5 +0,0 @@
-import { aliencrtActorSheet } from "../module/sheet/character-sheet.js";
-import { aliencrtSynthActorSheet } from "../module/sheet/synth-sheet.js";
-
-    Actors.registerSheet("alienrpg", aliencrtActorSheet, { types: ["character"], makeDefault: true }); //makeDefault: true is what makes this alternate sheet the default sheet when creating an "actor".
-	Actors.registerSheet("alienrpg", aliencrtSynthActorSheet, { types: ["synthetic"], makeDefault: true });
