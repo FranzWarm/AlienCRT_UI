d6a3cc787a49430052deb5fa14fdf3d7e723133e
Filip Steinhovden <filip.steinhovden@amedia.no>
2022-01-18 22:44:02 +0100

    fix: Sheets not set to default on initialization

--- dist/module/init.js
+++ dist/module/init.js
@@ -1,5 +1,22 @@
-import { aliencrtActorSheet } from "../module/sheet/character-sheet.js";
-import { aliencrtSynthActorSheet } from "../module/sheet/synth-sheet.js";
+Hooks.once("ready", () => {
+	const Character = CONFIG.Actor?.sheetClasses.character?.["alienrpg.alienrpgActorSheet"]?.cls;
+	const Synth = CONFIG.Actor?.sheetClasses.synthetic?.["alienrpg.alienrpgSynthActorSheet"]?.cls;
 
-    Actors.registerSheet("alienrpg", aliencrtActorSheet, { types: ["character"], makeDefault: true }); //makeDefault: true is what makes this alternate sheet the default sheet when creating an "actor".
+	class aliencrtActorSheet extends Character {
+
+		/** @override */
+		get template() {
+			return "modules/alien-crt-ui/templates/actor/character-sheet.html";
+		}
+	}
+	class aliencrtSynthActorSheet extends Synth {
+
+		/** @override */
+		get template() {
+			return "modules/alien-crt-ui/templates/actor/synth-sheet.html";
+		}
+	}
+
+	Actors.registerSheet("alienrpg", aliencrtActorSheet, { types: ["character"], makeDefault: true }); //makeDefault: true is what makes this alternate sheet the default sheet when creating an "actor".
 	Actors.registerSheet("alienrpg", aliencrtSynthActorSheet, { types: ["synthetic"], makeDefault: true });
+});
